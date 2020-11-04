29d0b1740f047391bc881e4b29795e6baa2d55f2
Jonathan Prieto <jonathan.prieto@me.com>
2020-11-04 10:09:51 -0500

    Test de Receiver

--- Suscriber/src/test/java/ReceiverTests.java
+++ Suscriber/src/test/java/ReceiverTests.java
@@ -1,9 +1,16 @@
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import lab.SuscriberApp;
+import org.testng.Assert;
 
 @SpringBootTest
-class ReceiverTests {
+class receiverTests {
     @Test
     void contextLoads() {
+    }
+    @org.testng.annotations.Test(invocationCount = 16, threadPoolSize = 16)
+    void maintest() {
+        SuscriberApp.main(new String[]{});
+        Assert.assertTrue(true,"MakeTest");
     }
 }
